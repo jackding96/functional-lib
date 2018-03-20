ecd5458dbf862699637b638c6bb242b41dd5c452
Jack Ding <jack.ding@rangle.io>
2018-03-20 14:54:26 -0400

    debounce attempt 1

---
+++ debounce.js
@@ -0,0 +1,23 @@
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+const func = function() {
+    console.log('FUNC called!');
+}
+const debounceFunc = debounce(func, 1000);
+
+while (1){
+    debounceFunc();
+}
